dcc277675a73f3333c48c85988f25e9b5b183317
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-30 20:40:48 -0800

    improve allowed origins logic

--- src/allowedOrigin.js
+++ src/allowedOrigin.js
@@ -1,11 +1,14 @@
 const allowedOrigin = allowed => ({
 	before: ({ callback, event: { headers: { origin } } }, next) => {
-		if (origin !== allowed) callback(null, {
-			headers: { 'Access-Control-Allow-Origin': '*' },
-			statusCode: 400,
-			body: JSON.stringify('Origin not allowed') // only matters for browser requests
-		})
-		else next()
+		if (origin.startsWith('https://') && origin.endsWith('ziro.app'))
+			next()
+		if (origin === allowed)
+			next()
+		else callback(null, {
+				headers: { 'Access-Control-Allow-Origin': '*' },
+				statusCode: 400,
+				body: JSON.stringify('Origin not allowed') // only matters for browser requests
+			})
 	}
 })
 
