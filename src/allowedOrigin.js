8262b86adf00e391245ae89db950992ce5864270
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-20 16:33:09 -0700

    fix rare error with allowedOrigin

--- src/allowedOrigin.js
+++ src/allowedOrigin.js
@@ -1,14 +1,23 @@
 const allowedOrigin = allowed => ({
 	before: ({ callback, event: { headers: { origin } } }, next) => {
-		if (origin.startsWith('https://') && origin.endsWith('ziro.app'))
-			next()
-		else if (origin === allowed)
-			next()
-		else callback(null, {
+		try {
+			if (origin.startsWith('https://') && origin.endsWith('ziro.app'))
+				next()
+			else if (origin === allowed)
+				next()
+			else callback(null, {
 				headers: { 'Access-Control-Allow-Origin': '*' },
 				statusCode: 400,
 				body: JSON.stringify('Origin not allowed') // only matters for browser requests
 			})
+		} catch (error) {
+			console.log(error)
+			callback(null, {
+				headers: { 'Access-Control-Allow-Origin': '*' },
+				statusCode: 400,
+				body: JSON.stringify('Origin header not present')
+			})
+		}
 	}
 })
 
